121d44132c66603da8488532c8762039d4d97563
opuusalu <opuusalu@gmail.com>
2021-09-21 10:24:09 +0300

    madis gai

--- stuff/Numero_uno.java
+++ stuff/Numero_uno.java
@@ -1,7 +1,26 @@
 package main.stuff;
+import java.util.Random;
 
 public class Numero_uno {
     public static void main(String[] args) {
-        System.out.println("Ma olen pro");
+
+        int n = 5;
+        int[][] matirix = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int num = new Random().nextInt(1000);
+                matirix[i][j] = num;
+            }
+
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matirix[i][j] + " ");
+            }
+            System.out.println("\n");
+
+        }
+
     }
 }
