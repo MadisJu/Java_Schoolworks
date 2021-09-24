8139d501e1e07838637810e434ab4f2135251c17
opuusalu <opuusalu@gmail.com>
2021-09-24 09:41:01 +0300

    Testing testing

--- Faililugemine/Oliver_Faililugemine.java
+++ Faililugemine/Oliver_Faililugemine.java
@@ -1,11 +1,24 @@
 package main.Faililugemine;
 
 import java.io.File;
+import java.util.Scanner;
 
 public class Oliver_Faililugemine {
 
     public static void main(String[] args) {
-        File file = new File();
+        try {
+            File file = new File("C:\\Users\\oliver.puusalu\\IdeaProjects\\Java_Schoolworks\\Faililugemine\\Fail.txt");
+
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNext()) {
+                System.out.println(scanner.nextLine());
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Error");
+        }
+
     }
 
 }
