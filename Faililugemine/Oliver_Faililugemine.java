41b8f028c8b2c2dde6301b20b0b3d79400605b97
opuusalu <opuusalu@gmail.com>
2021-09-24 09:34:56 +0300

    Testing testing

--- Faililugemine/Oliver_Faililugemine.java
+++ Faililugemine/Oliver_Faililugemine.java
@@ -1,4 +1,11 @@
 package main.Faililugemine;
 
+import java.io.File;
+
 public class Oliver_Faililugemine {
+
+    public static void main(String[] args) {
+        File file = new File();
+    }
+
 }
