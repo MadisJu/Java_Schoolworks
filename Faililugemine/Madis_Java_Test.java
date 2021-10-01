79148d8e408e8ce915e75970a3c620fadf737d0b
Madis <estonira@gmail.com>
2021-10-01 09:28:12 +0300

    Kontrolltöö part 2

--- Faililugemine/Madis_Java_Test.java
+++ Faililugemine/Madis_Java_Test.java
@@ -8,10 +8,17 @@ import java.util.Scanner;
 
 public class Madis_Java_Test {
 
+    //Oh jesus lord i dont like the size of this code, maybe its normal but i think its not, couldve done it more effiently defenetyl
+    //Just coudlve needed more time i think.
+    //Maybe im not too sure.
+
+
+    public static List<String> nimed = new ArrayList<>();
+
     public static int[][] Alagrupi_Tulemused() throws FileNotFoundException
     {
 
-        File fail = new File("C:\\Users\\madis.tamberg\\IdeaProjects\\Java_Schoolworks\\Faililugemine\\Fail.txt");
+        File fail = new File("C:\\Users\\madis.tamberg\\IdeaProjects\\Java_Schoolworks\\Faililugemine\\Alagrupp.txt");
 
         Scanner lugeja = new Scanner(fail);
 
@@ -38,6 +45,7 @@ public class Madis_Java_Test {
         }
 
         lugeja.close();
+        nimed = Nimed;
         //Nimed lõpp
 
         int[][] tulemus_maatrix = new int[Nimed.size()][Nimed.size()];
@@ -52,19 +60,60 @@ public class Madis_Java_Test {
             jaotatud[1] = tulemused[0];
             tulemused = tulemused[1].split(":");
 
-            if(tulemused[0] > tulemused[1])
+            //Eelnevalt on nimed olemas, tulemused ka, vasakpoolne ja parempoolne
+            //Selle järgi vaadata kumb pool suurem on ja seejärel küsida Nimede listist.
+
+            if(Integer.parseInt(tulemused[0]) > Integer.parseInt(tulemused[1]))
             {
-                tulemus_maatrix[][] =
+                tulemus_maatrix[Nimed.indexOf(jaotatud[0])][Nimed.indexOf(jaotatud[1])] = 2;
+                tulemus_maatrix[Nimed.indexOf(jaotatud[1])][Nimed.indexOf(jaotatud[0])] = 0;
+            }
+            else if(Integer.parseInt(tulemused[1]) > Integer.parseInt(tulemused[0]))
+            {
+                tulemus_maatrix[Nimed.indexOf(jaotatud[1])][Nimed.indexOf(jaotatud[0])] = 2;
+                tulemus_maatrix[Nimed.indexOf(jaotatud[0])][Nimed.indexOf(jaotatud[1])] = 0;
+            }
+            else if(Integer.parseInt(tulemused[1]) == Integer.parseInt(tulemused[0]))
+            {
+                tulemus_maatrix[Nimed.indexOf(jaotatud[1])][Nimed.indexOf(jaotatud[0])] = 1;
+                tulemus_maatrix[Nimed.indexOf(jaotatud[0])][Nimed.indexOf(jaotatud[1])] = 1;
             }
 
-
-
-
         }
-        return;
+        return tulemus_maatrix;
     }
 
-    public static void main(String[] args) {
+    public static int suurima_maatrixi_index(int[][] matrix) //Pm leiab Array summa ja kui on suurem siis salvestab selle ära see järel tagastab indexi suurima array oma.
+    {
+        int iterator = 0;
+        int suurim_summa = 0;
+
+        for (int i = 0; i < matrix.length - 1; i++) {
+            int summa = 0;
+            for (int j :
+                    matrix[i]) {
+                summa += j;
+            }
+            if(summa > suurim_summa) {suurim_summa = summa; iterator = i;}
+        }
+
+        return iterator;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        int[][] TulemusMaatrix = Alagrupi_Tulemused();
+
+        for (int[] maatriksi_rida: TulemusMaatrix)
+        {
+            for (int tulemus: maatriksi_rida)
+            {
+                System.out.print(tulemus + " ");
+            }
+            System.out.print("\n");
+        }
+
+        System.out.println("Alagrupi võitis: " + nimed.get(suurima_maatrixi_index(TulemusMaatrix)));
 
     }
 }
