8ffcf4af42c2cd8bc053ca5500de1e6d0c10b00d
opuusalu <opuusalu@gmail.com>
2021-10-19 10:22:50 +0300

    Push 19/10

---
+++ OOP_oliver/Lapsehoidja.java
@@ -0,0 +1,28 @@
+package main.OOP_oliver;
+
+public class Lapsehoidja {
+
+    private int company_num;
+    private String name;
+
+    public Lapsehoidja(int company_num, String name) {
+        this.company_num = company_num;
+        this.name = name;
+    }
+
+    public int getCompany_num() {
+        return company_num;
+    }
+
+    public void setCompany_num(int company_num) {
+        this.company_num = company_num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
