02e7c2a57bbd3824215864ce448a96dbe6a5c32a
opuusalu <opuusalu@gmail.com>
2021-10-19 10:44:50 +0300

    Push 19/10

---
+++ stuff/get_vektored.java
@@ -0,0 +1,26 @@
+package main.stuff;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Vector;
+
+public class get_vektored {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> test = new ArrayList<>();
+
+        test.add(5);
+        test.add(3);
+        test.add(7);
+        test.add(1);
+
+        Collections.sort(test);
+
+        for (Integer i: test) {
+            System.out.println(i);
+        }
+
+
+    }
+}
