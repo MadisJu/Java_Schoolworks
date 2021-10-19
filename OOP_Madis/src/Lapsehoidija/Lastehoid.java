240e0bc6fa1dfffad0b6e5b5f23ed955ba4637c4
Madis <estonira@gmail.com>
2021-10-19 10:13:20 +0300

    First commit - Finished the class and data

---
+++ OOP_Madis/src/Lapsehoidija/Lastehoid.java
@@ -0,0 +1,11 @@
+package main.OOP_Madis.src.Lapsehoidija;
+
+import java.io.File;
+
+public class Lastehoid {
+
+    public static void main(String[] args) {
+
+        File f = new File()
+    }
+}
