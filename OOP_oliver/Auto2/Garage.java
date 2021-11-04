37a442e7b711b847caf843487e87e91075d224aa
MadisJu <estonira@gmail.com>
2021-11-04 22:24:36 +0200

    Fixed Oliver's stuff

--- OOP_oliver/Auto2/Garage.java
+++ OOP_oliver/Auto2/Garage.java
@@ -1,6 +1,7 @@
 package main.OOP_oliver.Auto2;
 import main.OOP_oliver.Auto.Car;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class Garage {
@@ -27,33 +28,37 @@ public class Garage {
 
     public void output_by_brand(String brand) {
 
-        for (Car2 car : cars
-             ) {
-            if (brand.equals(car.getBrand())) {
+        for (Car2 car : cars)
+        {
+            if (brand.equals(car.getBrand()))
+            {
                 System.out.println(car.car_data());
             }
-                                                                                                        }
-                                                                                                        }
+        }
+    }
 
-    public void output_by_colour(String colour)                                                         {
+    public void output_by_colour(String colour)
+    {
 
-        for (Car2 car : cars
-             )                                                                                          {
-            if (colour.equals(car.getColour()))                                                         {
+        for (Car2 car : cars)
+        {
+            if (colour.equals(car.getColour()))
+            {
                 System.out.println(car.car_data());
-                                                                                                        }
-                                                                                                        }
+            }
+        }
+    }
 
-                                                                                                        }
-
-    public void output_by_speed()                                                              {
+    public void output_by_speed(){
 
         ArrayList<Integer> list_speeds = new ArrayList<>();
 
-        for (Car2 car : cars
-             )                                                                                          {
+        for (Car2 car : cars)
+        {
             list_speeds.add(car.getMax_speed());
-                                                                                                        }
+        }
+
+        ArrayList<Integer> Copy_of_list_speeds = (ArrayList<Integer>) list_speeds.clone();
 
         Collections.sort(list_speeds, Collections.reverseOrder());
 
@@ -62,10 +67,10 @@ public class Garage {
 
             System.out.println(list_speeds.get(i));
 
-            System.out.println(cars.get(list_speeds.indexOf(list_speeds.get(i))).car_data());
+            System.out.println(cars.get(Copy_of_list_speeds.indexOf(list_speeds.get(i))).car_data());
 
             //System.out.println(cars.get(list_speeds.indexOf(temp)).car_data());
 
-                                                                                                        }
-                                                                                                        }
-                                                                                                        }
+        }
+    }
+}
